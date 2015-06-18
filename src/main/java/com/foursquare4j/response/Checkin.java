009367a65311769dac095f3cd264c34a2beaf54c
dan-zx <danyboy_shaggy@hotmail.com>
2015-06-18 11:29:22 -0500

    License fixes

--- src/main/java/com/foursquare4j/response/Checkin.java
+++ src/main/java/com/foursquare4j/response/Checkin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Daniel Pedraza-Arcega
+ * Copyright 2014-2015 Daniel Pedraza-Arcega
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
